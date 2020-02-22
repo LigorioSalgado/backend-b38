aaacebef58760292e8667cbd914a124477209eff
ligorio93 <edwin@devf.mx>
2020-02-21 19:51:09 -0600

    crud users complete

--- server/resolvers/Mutation/UserMutation.js
+++ server/resolvers/Mutation/UserMutation.js
@@ -6,6 +6,13 @@ module.exports = {
 
 	createUser:(root,args) => {
 		return Users.create(args.data);
+	},
+	updateUser:(root,args) => {
+		return Users.findByIdAndUpdate(args.id,{$set:{...args.data}},{new:true});
+
+	},
+	deleteUser:(root,args) => {
+		return Users.findByIdAndUpdate(args.id, {$set:{is_active:false}},{new:true});
 	}
 
 };
